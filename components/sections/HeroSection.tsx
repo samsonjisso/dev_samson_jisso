48cc650b589ac99063c87e324b80085ec74a2845
= <=>
2026-06-15 09:05:48 -0400

    Remove console log from HeroSection component

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -20,7 +20,6 @@ const HERO_QUERY = defineQuery(`*[_id == "singleton-profile"][0]{
 
 async function  HeroSection() {
     const {data: profile} = await sanityFetch({query: HERO_QUERY})
-    console.log(profile);
   return (
     <div>HeroSection</div>
   )
